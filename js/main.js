78d3f524bbe6b561ebab7b93f9c18fd95fe402b9
b7c8p <morapaezc@gmail.com>
2021-10-04 22:32:01 -0500

    Avance en validaciones js y estructura inicial html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log("Hola");
+const createComment = () => {
+
+  const data  = [{
+    titleTask: "",
+    contentTask: "",
+  }]
+
+  document.querySelector('button').addEventListener( 'click',() => {
+    const listTasks = document.getElementById('taskContainer');
+    const nameTask = document.getElementById('task');
+    const contentTask = document.getElementById('taskDescription');
+    const error = document.getElementById('taskError')
+    if(nameTask.value === '') {
+      error.innerHTML = "Debes ingresar el nombre";
+    } else {
+      error.innerHTML = "";
+    }
+  });
+
+}
+
+createComment();
