31bb005ca8d6aee25dddf06c047214ae9287b9cb
b7c8p <morapaezc@gmail.com>
2021-10-05 21:29:00 -0500

    funcion addTask casi finalizada

--- js/main.js
+++ js/main.js
@@ -1,22 +1,50 @@
-const createComment = () => {
+const app = () => {
+  // Variables
+  const nameTask = document.getElementById('task');
+  const contentTask = document.getElementById('taskDescription');
+  const listTitle = document.getElementById('title')
+  const listContent = document.getElementById('content')
 
-  const data  = [{
-    titleTask: "",
-    contentTask: "",
-  }]
+  // Funcion añadir tarea
+  const addTask = () => {
+    // Textos 'nodos'
+    const task = document.createTextNode(nameTask.value);
+    const content = document.createTextNode(contentTask.value);
 
-  document.querySelector('button').addEventListener( 'click',() => {
-    const listTasks = document.getElementById('taskContainer');
-    const nameTask = document.getElementById('task');
-    const contentTask = document.getElementById('taskDescription');
-    const error = document.getElementById('taskError')
-    if(nameTask.value === '') {
-      error.innerHTML = "Debes ingresar el nombre";
-    } else {
-      error.innerHTML = "";
+    // Validacion de inputs
+    // Se le da el atributo placeholder con su valor
+    // Se agrega una clase no implicita en el codigo html original
+    //    Si no cuando se cumpla la condicion se agrega dicha clase
+    if (nameTask.value === "" ) {
+      nameTask.setAttribute("placeholder", "Task invalid");
+      nameTask.className = "error"
+      return false;
     }
-  });
+
+    if (contentTask.value === "") {
+      contentTask.setAttribute("placeholder", "Content invalid");
+      contentTask.className = "error"
+      return false;
+    }
+
+    // Se crea los elemenos
+
+    const newTask = document.createElement('li');
+    const newContent = document.createElement('li');
+
+    // Se agrega el contenido al elemento y se agrega el elemento al padre
+    newTask.appendChild(task);
+    newContent.appendChild(content);
+    listTitle.appendChild(newTask);
+    listContent.appendChild(newContent);
+  }
+
+
+  document.querySelector('button').addEventListener( 'click', addTask);
 
 }
 
-createComment();
+  // error.innerHTML = "";
+  // document.getElementById('title').append(nameTask.value);
+  // document.getElementById('content').append(contentTask.value);
+app();
